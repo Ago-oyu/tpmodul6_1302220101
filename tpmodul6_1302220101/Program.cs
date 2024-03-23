78af73d7a0e76f0a1c489b6d7bef1d8f9574ae0f
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2024-03-24 01:05:12 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302220101/Program.cs
+++ tpmodul6_1302220101/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302220101
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Yoga Fikri_Praktikan]");
+            video.PrintVideoDetails();
+        }
+    }
+}
---
+++ tpmodul6_1302220101/SayaTubeVideo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302220101
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            this.title = title;
+            playCount = 0;
+            Random rd = new Random();
+            id = rd.Next(10000, 99999);
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("id         : " + id);
+            Console.WriteLine("title      : " + title);
+            Console.WriteLine("play count : " + playCount);
+        }
+    }
+}
