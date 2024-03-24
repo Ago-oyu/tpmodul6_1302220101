7126fec40f3972a7dc41c2ef6341af17bf8a2998
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2024-03-24 15:48:46 +0700

    implementasi design by contract

--- tpmodul6_1302220101/Program.cs
+++ tpmodul6_1302220101/Program.cs
@@ -10,8 +10,20 @@ namespace tpmodul6_1302220101
     {
         public static void Main(string[] args)
         {
-            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Yoga Fikri_Praktikan]");
-            video.PrintVideoDetails();
+            SayaTubeVideo video1 = new SayaTubeVideo("");
+            SayaTubeVideo video2 = new SayaTubeVideo("Kg6N4TQsHpP9bMYYfr667YNFgvmczjJDFcvraJqQwwxvAwJu3kwBsyTqpH4TcE4PC7dkXJvEEykrh9tAzDQJHAqrSu6DH7ttkDgWy");
+            SayaTubeVideo video3 = new SayaTubeVideo("Tutorial Design By Contract - [Yoga Fikri_Praktikan]");
+
+            video3.PrintVideoDetails();
+            video3.IncreasePlayCount(10000001);
+            video3.IncreasePlayCount(100);
+            video3.PrintVideoDetails();
+
+            for (int i = 0;i < 215;i++)
+            {
+                video3.IncreasePlayCount(10000000);
+            }
+            video3.PrintVideoDetails();
         }
     }
 }
--- tpmodul6_1302220101/SayaTubeVideo.cs
+++ tpmodul6_1302220101/SayaTubeVideo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,22 +16,65 @@ namespace tpmodul6_1302220101
 
         public SayaTubeVideo(String title)
         {
-            this.title = title;
-            playCount = 0;
-            Random rd = new Random();
-            id = rd.Next(10000, 99999);
+            try
+            {
+                if (String.IsNullOrEmpty(title))
+                {
+                    throw new ArgumentNullException();
+                } else if (title.Length > 100)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.title = title;
+                playCount = 0;
+                Random rd = new Random();
+                id = rd.Next(10000, 99999);
+
+            }
+            
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message + " (title tidak boleh null atau kosong)\n");
+            }
+            
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message + " (title tidak boleh lebih dari 100 karakter)\n");
+            }
+           
         }
 
         public void IncreasePlayCount(int playCount)
         {
-            this.playCount += playCount;
+
+            try
+            {
+                if (playCount > 10000000)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.playCount = checked(this.playCount + playCount);
+            }
+            
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message + " (input playCount tidak boleh lebih dari 10.000.000)\n");
+            }
+
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+            }
+            
         }
 
         public void PrintVideoDetails()
         {
             Console.WriteLine("id         : " + id);
             Console.WriteLine("title      : " + title);
-            Console.WriteLine("play count : " + playCount);
+            Console.WriteLine("play count : " + playCount + "\n");
         }
     }
 }
